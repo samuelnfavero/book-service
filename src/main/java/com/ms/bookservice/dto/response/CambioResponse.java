319efaf6cdee545b8871d4cccc1a406b857aa511
samuelnfavero <faveronsamuel@gmail.com>
2022-05-16 14:28:31 -0300

    feat:criacao do dto cambio response para receber dados da api de cambio

--- src/main/java/com/ms/bookservice/dto/response/CambioResponse.java
+++ src/main/java/com/ms/bookservice/dto/response/CambioResponse.java
@@ -1,2 +1,15 @@
-package com.ms.bookservice.dto.response;public class CambioResponse {
+package com.ms.bookservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor @Getter
+public class CambioResponse {
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String enviroment;
 }
