a503c88df94144f99dbdadc3c9c1170d68f01f60
samuelnfavero <faveronsamuel@gmail.com>
2022-05-17 10:11:18 -0300

    feat:Adicao de mais moedas

--- src/main/java/com/ms/bookservice/enums/CurrencyEnum.java
+++ src/main/java/com/ms/bookservice/enums/CurrencyEnum.java
@@ -5,9 +5,13 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum CurrencyEnum {
     BRL("Real"),
+    EUR("Euro"),
+    GBP("Libra Esterlina"),
+    ARS("Peso Argentino"),
+    CLP("Peso Chileno"),
+    COP("Peso Colombiano"),
+    MXN("Peso Mexicano"),
     USD("Dólar");
 
     public final String currency;
-
-
 }
